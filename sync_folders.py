8c155d9f121e3a8c3d7d09ff80b72c3ba29b8c65
Alex Enache <alexenache71@gmail.com>
2023-04-05 16:12:11 +0300

    First fully functional version of the Folder Sync program.

--- sync_folders.py
+++ sync_folders.py
@@ -3,46 +3,15 @@ import shutil
 import time
 import argparse
 import logging
-import hashlib
 
 time_now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-
-
-def compare_files(file1, file2):
-    # This function compares two files with their hashes. We will open the files only for reading in a binary format,
-    # or rb for short.
-    with open(file1, 'rb') as f1:
-        with open(file2, 'rb') as f2:
-            if hashlib.md5(f1.read()).hexdigest() == hashlib.md5(f2.read()).hexdigest():
-                return True
-            else:
-                return False
-
-
-def compare_folders(source, replica):
-    # compare hash source
-    # it either returns True if all files are the same, or false if any of the files are different.
-    # get all files in source
-    files_source = os.listdir(source)
-    # get all files in replica
-    files_replica = os.listdir(replica)
-    # compare 2 list
-    if len(files_source) != len(files_replica):
-        return False
-
-    for file in files_source:
-        if file in files_replica:
-            if not compare_files(source + '/' + file, replica + '/' + file):
-                return False
-        else:
-            return False
-    return True
 
 
 def synchronize_folders(source_path, replica_path, log_path, interval):
     logging.basicConfig(filename=log_path, level=logging.INFO)
 
     while True:
+        print("Synchronization started at " + time_now)
         logging.info("Synchronization started at %s", time_now)
 
         for dirpath, dirnames, filenames in os.walk(source_path):
@@ -51,6 +20,7 @@ def synchronize_folders(source_path, replica_path, log_path, interval):
 
             if not os.path.exists(replica_dirpath):
                 os.makedirs(replica_dirpath)
+                print("Created directory " + replica_dirpath)
                 logging.info("Created directory %s", replica_dirpath)
 
             for filename in filenames:
@@ -60,9 +30,11 @@ def synchronize_folders(source_path, replica_path, log_path, interval):
                 if os.path.exists(replica_filepath):
                     if os.path.getmtime(source_filepath) > os.path.getmtime(replica_filepath):
                         shutil.copy2(source_filepath, replica_filepath)
+                        print("Copied " + source_filepath + " to " + replica_filepath)
                         logging.info("Copied %s to %s", source_filepath, replica_filepath)
                 else:
                     shutil.copy2(source_filepath, replica_filepath)
+                    print("Copied " + source_filepath + " to " + replica_filepath)
                     logging.info("Copied %s to %s", source_filepath, replica_filepath)
 
             for dirname in dirnames:
@@ -71,6 +43,7 @@ def synchronize_folders(source_path, replica_path, log_path, interval):
 
                 if not os.path.exists(replica_dirpath):
                     os.makedirs(replica_dirpath)
+                    print("Created directory " + replica_dirpath)
                     logging.info("Created directory %s", replica_dirpath)
 
         for dirpath, dirnames, filenames in os.walk(replica_path):
@@ -79,6 +52,7 @@ def synchronize_folders(source_path, replica_path, log_path, interval):
 
             if not os.path.exists(source_dirpath):
                 shutil.rmtree(dirpath)
+                print("Removed directory " + dirpath)
                 logging.info("Removed directory %s", dirpath)
 
             for filename in filenames:
@@ -87,8 +61,10 @@ def synchronize_folders(source_path, replica_path, log_path, interval):
 
                 if not os.path.exists(source_filepath):
                     os.remove(replica_filepath)
+                    print("Removed file " + replica_filepath)
                     logging.info("Removed file %s", replica_filepath)
 
+        print("Synchronization ended at " + time_now)
         logging.info("Synchronization ended at %s", time_now)
 
         time.sleep(interval)
